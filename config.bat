d221072b9abdff213a3868619a7530992cd76743
Anne <avdg81@users.noreply.github.com>
2024-02-07 13:25:47 +0100

    Moved @echo off to the top of the script

--- config.bat
+++ config.bat
@@ -1,5 +1,6 @@
-REM A tiny script to resemble the `config` alias as described by the article
+@echo off
+
+REM A small script to resemble the `config` alias as described by the article
 REM "Dotfiles: Best way to store in a bare git repository"
 REM (https://www.atlassian.com/git/tutorials/dotfiles)
-@echo off
 git --git-dir=%HOMEDRIVE%%HOMEPATH%\.cfg\ --work-tree=%HOMEDRIVE%%HOMEPATH% %*
